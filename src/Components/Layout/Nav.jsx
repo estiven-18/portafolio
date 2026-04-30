a17d19bb1a7efa7676219e0b1557d1061c0a8f96
estiven-18 <nelsoncuartas21@gmail.com>
2026-04-30 07:04:50 -0500

    Update Nav.jsx

--- src/Components/Layout/Nav.jsx
+++ src/Components/Layout/Nav.jsx
@@ -1,20 +1,39 @@
 const Nav = () => {
   return (
-    <nav className="mx-auto mt-4  w-fit border-b border-white px-2 pb-3">
+    <nav className="mx-auto mt-4 w-fit border-b border-white px-2 pb-3">
       <ul className="flex items-center justify-center gap-10 text-base text-white">
         <li>
-          <a href="/" className="pb-1 transition-colors hover:text-lime-400"><b>Inicio</b></a>
+          <a
+            href="#PantallaPrincipal"
+            className="pb-1 transition-colors hover:text-lime-400"
+          >
+            <b>Inicio</b>
+          </a>
         </li>
         <li>
-          <a href="#about" className="pb-1 transition-colors hover:text-lime-400"><b>Sobre mí</b></a>
+          <a
+            href="#PantallaAbout"
+            className="pb-1 transition-colors hover:text-lime-400"
+          >
+            <b>Sobre mí</b>
+          </a>
         </li>
         <li>
-          <a href="/skills" className="pb-1 transition-colors hover:text-lime-400"><b>Habilidades</b></a>
+          <a
+            href="#PantallaSkills"
+            className="pb-1 transition-colors hover:text-lime-400"
+          >
+            <b>Habilidades</b>
+          </a>
         </li>
         <li>
-          <a href="/projects" className="pb-1 transition-colors hover:text-lime-400"><b>Proyectos</b></a>
+          <a
+            href="#PantallaProyects"
+            className="pb-1 transition-colors hover:text-lime-400"
+          >
+            <b>Proyectos</b>
+          </a>
         </li>
-        
       </ul>
     </nav>
   );
