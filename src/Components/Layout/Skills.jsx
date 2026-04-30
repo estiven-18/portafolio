9833637515f69abc6d5777ad2b9309fcd169359a
estiven-18 <nelsoncuartas21@gmail.com>
2026-04-30 07:05:55 -0500

    skills
    Refactor layout and styling for Skills component.

--- src/Components/Layout/Skills.jsx
+++ src/Components/Layout/Skills.jsx
@@ -11,19 +11,22 @@ const Skills = () => {
   }, []);
 
   return (
-    <div className="m-18">
-      <h1 className="flex justify-center text-lime-400 text-5xl">
+    <div className="py-20 px-4 flex flex-col justify-center items-center text-center max-w-4xl mx-auto">
+      <h1 className="text-lime-400 text-5xl mb-6">
         <b>Habilidades</b>
       </h1>
-      <p className="mt-4 text-center text-lg text-white/75">
+      <p className="text-center text-lg text-white/75 max-w-2xl mb-12">
         Desarrollo web, APIs y backend.
       </p>
-      <div className="flex flex-col md:flex-row items-center justify-center">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4 max-w-4xl w-full">
         {info.map((card) => (
-          <CardSkills key={card.title} title={card.title} skills={card.skills} />
+          <CardSkills
+            key={card.title}
+            title={card.title}
+            skills={card.skills}
+          />
         ))}
       </div>
-
     </div>
   );
 };
