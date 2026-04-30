8bedb74e09870a238ec1877a8023b637d4a50d98
estiven-18 <nelsoncuartas21@gmail.com>
2026-04-30 07:07:21 -0500

    cardabout

--- src/Components/Pages/CardAbout.jsx
+++ src/Components/Pages/CardAbout.jsx
@@ -1,10 +1,10 @@
 const CardAbout = ({ number, descripcion }) => {
   return (
-    <div className="flex items-center border-white/6 justify-between gap-4 rounded-2xl bg-white/10 p-6 backdrop-blur-md hover:border-lime-400 border-2">
-      <div className="flex h-12 w-12 shrink-0 items-center justify-center rounded-xl border border-white/40 bg-white/90 text-sm font-semibold text-black">
+    <div className="flex flex-col items-center justify-center gap-4 rounded-2xl bg-white/10 p-6 backdrop-blur-md hover:border-lime-400 border-2 border-white/6 text-center">
+      <div className="flex h-14 w-14 shrink-0 items-center justify-center rounded-xl border border-white/40 bg-white/90 text-sm font-semibold text-black">
         <a>{number}</a>
       </div>
-      <p className="text-left text-sm text-white/75">{descripcion}</p>
+      <p className="text-sm text-white/75">{descripcion}</p>
     </div>
   );
 };
