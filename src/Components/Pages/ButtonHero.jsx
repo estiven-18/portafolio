a83af9d62709c01b7542e2e660c1d238e05648e9
estiven-18 <nelsoncuartas21@gmail.com>
2026-04-30 07:07:00 -0500

    buttonherro

--- src/Components/Pages/ButtonHero.jsx
+++ src/Components/Pages/ButtonHero.jsx
@@ -2,7 +2,7 @@ const ButtonHero = ({ text, href }) => {
   return (
     <a
       href={href}
-      className="mt-6 inline-flex rounded-2xl border border-white/40 bg-white/20 px-5 py-2 text-white backdrop-blur-md transition-transform hover:bg-lime-400 hover:text-black"
+      className=" inline-flex rounded-2xl border border-white/40 bg-white/20 px-5 py-2 text-white backdrop-blur-md transition-transform hover:bg-lime-400 hover:text-black"
     >
       {text}
     </a>
