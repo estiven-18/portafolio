db46641279d974565a9ce2464611771b32ecd89d
estiven-18 <nelsoncuartas21@gmail.com>
2026-04-30 07:03:05 -0500

    about

--- src/Components/Layout/About.jsx
+++ src/Components/Layout/About.jsx
@@ -11,22 +11,22 @@ const About = () => {
   }, []);
 
   return (
-    <div className="m-18">
-      <h1 className="flex justify-center text-lime-400 text-5xl">
+    <div className="py-20 px-4 flex flex-col justify-center items-center text-center max-w-4xl mx-auto">
+      <h1 className="text-lime-400 text-5xl mb-6">
         <b>Sobre mí</b>
       </h1>
-      <p className="mt-4 text-center text-lg text-white/75 p">
+      <p className="text-center text-lg text-white/75 p max-w-2xl mb-12">
         Soy programador Full-Stack. Actualmente soy aprendiz del SENA en
-        análisis y desarrollo de software. Me apasiona crear
-        aplicaciones web modernas, escalables y eficientes. Siempre busco
-        aprender nuevas tecnologías y mejorar mis habilidades. Disfruto
-        colaborar en proyectos, explorar nuevas herramientas y me gustaria contribuir al
-        crecimiento del software colombiano.
+        análisis y desarrollo de software. Me apasiona crear aplicaciones web
+        modernas, escalables y eficientes. Siempre busco aprender nuevas
+        tecnologías y mejorar mis habilidades. Disfruto colaborar en proyectos,
+        explorar nuevas herramientas y me gustaria contribuir al crecimiento del
+        software colombiano.
       </p>
-      <h1 className="flex justify-center text-2xl mt-8 text-lime-400">
+      <h1 className="text-2xl text-lime-400 mb-8">
         <b>Mi Enfoque</b>
       </h1>
-      <div className="mt-8 grid grid-cols-1 md:grid-cols-3 gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4 max-w-4xl w-full">
         {info.map((card) => (
           <CardAbout
             key={card.id}
